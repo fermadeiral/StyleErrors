98376539e2dc58a005020a517ddf0c1355e123eb
Fer Madeiral <fer.madeiral@gmail.com>
2019-10-17 09:26:40 -0400

    [Automatic commit] Errors from americanexpress/nodes

---
+++ americanexpress-nodes/3091930563d8d3cda0308ccd3b98dfa1eb68d593/0/GraphQLRequestEntityTest.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright (c) 2018 American Express Travel Related Services Company, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.aexp.nodes.graphql;
+
+import org.junit.Before;
+import org.junit.Test;
+import io.aexp.nodes.graphql.models.TestModel;
+import io.aexp.nodes.graphql.models.TestModelEnum;
+import io.aexp.nodes.graphql.models.TestModelOptionalArguments;
+import io.aexp.nodes.graphql.models.TestModelScalar;
+
+import java.math.BigDecimal;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class GraphQLRequestEntityTest {
+
+    enum STATUS {
+        active
+    }
+
+    private static String EXAMPLE_URL = "https://graphql.example.com";
+    private List<String> idList;
+
+    @Before
+    public void setUp() {
+        idList = new ArrayList<String>();
+        idList.add("47");
+        idList.add("$id");
+    }
+
+    @Test
+    public void requestWithoutUrl() {
+        IllegalStateException exception = null;
+        try {
+            GraphQLRequestEntity.Builder().request(TestModel.class).build();
+        } catch (IllegalStateException e) {
+            exception = e;
+        }
+        assertNotNull(exception);
+        assertEquals("url must be set", exception.getMessage());
+    }
+
+    @Test
+    public void requestWithoutRequestBody() throws MalformedURLException {
+        IllegalStateException exception = null;
+        try {
+            GraphQLRequestEntity.Builder().url(EXAMPLE_URL).build();
+        } catch (IllegalStateException e) {
+            exception = e;
+        }
+        assertNotNull(exception);
+        assertEquals("request must be set", exception.getMessage());
+    }
+
+    @Test
+    public void requestWithOptionalParameter() throws MalformedURLException {
+        GraphQLRequestEntity requestEntity = GraphQLRequestEntity.Builder()
+                .url(EXAMPLE_URL)
+                .request(TestModelOptionalArguments.class)
+                .arguments(
+                        new Arguments("test.nested",
+                                new Argument<List<String>>("ids", idList),
+                                new Argument<Integer>("first", 10)))
+                .build();
+        requestEntity.setRequestMethod(GraphQLTemplate.GraphQLMethod.QUERY);
+        assertEquals(EXAMPLE_URL, requestEntity.getUrl().toString());
+        assertEquals("GraphQLRequestEntity{request='query { test { nested (ids:[\"47\",$id],first:10) { string } } } ', url='"+EXAMPLE_URL+"'}", requestEntity.toString());
+    }
+
+    @Test
+    public void requestWithOtherOptionalParameter() throws MalformedURLException {
+        GraphQLRequestEntity requestEntity = GraphQLRequestEntity.Builder()
+                .url(EXAMPLE_URL)
+                .request(TestModelOptionalArguments.class)
+                .arguments(
+                        new Arguments("test.nested",
+                                new Argument<List<String>>("ids", idList),
+                                new Argument<Integer>("last", 5)))
+                .build();
+        requestEntity.setRequestMethod(GraphQLTemplate.GraphQLMethod.QUERY);
+        assertEquals(EXAMPLE_URL, requestEntity.getUrl().toString());
+        assertEquals("GraphQLRequestEntity{request='query { test { nested (ids:[\"47\",$id],last:5) { string } } } ', url='"+EXAMPLE_URL+"'}", requestEntity.toString());
+    }
+
+    @Test
+    public void requestWithEnumInModel() throws MalformedURLException {
+        GraphQLRequestEntity requestEntity = GraphQLRequestEntity.Builder()
+                .url(EXAMPLE_URL)
+                .request(TestModelEnum.class)
+                .build();
+        requestEntity.setRequestMethod(GraphQLTemplate.GraphQLMethod.QUERY);
+        assertEquals(EXAMPLE_URL, requestEntity.getUrl().toString());
+        assertEquals("GraphQLRequestEntity{request='query { number } ', url='"+EXAMPLE_URL+"'}", requestEntity.toString());
+    }
+
+    @Test
+    public void createSimpleRequest() throws MalformedURLException {
+        GraphQLRequestEntity requestEntity = GraphQLRequestEntity.Builder()
+                .url(EXAMPLE_URL)
+                .request(TestModel.class)
+                .build();
+        requestEntity.setRequestMethod(GraphQLTemplate.GraphQLMethod.QUERY);
+        assertEquals(EXAMPLE_URL, requestEntity.getUrl().toString());
+        assertEquals("GraphQLRequestEntity{request='query ($andAnothaVariable:status,$anothaVariable:Int,$andAListVariable:[String],$variableName:String!){ test (id:null) { testShort : testShort testCharacter testList { anotherTestString (variableName:$variableName) andAnothaOne (anothaVariable:$anothaVariable,andAnothaVariable:$andAnothaVariable,andAListVariable:$andAListVariable) } testInteger testBoolean nestedTest { anotherTestString (variableName:$variableName) andAnothaOne (anothaVariable:$anothaVariable,andAnothaVariable:$andAnothaVariable,andAListVariable:$andAListVariable) } testByte : testByte testString : testString (anotherOne:null,default:\"default\",defaultList:null) testArrayList testFloat testDouble testLong } } ', url='"+EXAMPLE_URL+"'}", requestEntity.toString());
+    }
+
+    @Test
+    public void simpleRequestWithUrl() throws MalformedURLException {
+        GraphQLRequestEntity requestEntity = GraphQLRequestEntity.Builder()
+                .url(EXAMPLE_URL)
+                .request(TestModel.class)
+                .build();
+        assertEquals(EXAMPLE_URL, requestEntity.getUrl().toString());
+        assertEquals("GraphQLRequestEntity{request='query ($andAnothaVariable:status,$anothaVariable:Int,$andAListVariable:[String],$variableName:String!){ test (id:null) { testShort : testShort testCharacter testList { anotherTestString (variableName:$variableName) andAnothaOne (anothaVariable:$anothaVariable,andAnothaVariable:$andAnothaVariable,andAListVariable:$andAListVariable) } testInteger testBoolean nestedTest { anotherTestString (variableName:$variableName) andAnothaOne (anothaVariable:$anothaVariable,andAnothaVariable:$andAnothaVariable,andAListVariable:$andAListVariable) } testByte : testByte testString : testString (anotherOne:null,default:\"default\",defaultList:null) testArrayList testFloat testDouble testLong } } ', url='"+EXAMPLE_URL+"'}", requestEntity.toString());
+    }
+
+    @Test
+    public void simpleRequestWithMethod() throws MalformedURLException {
+        GraphQLRequestEntity requestEntity = GraphQLRequestEntity.Builder()
+                .url(EXAMPLE_URL)
+                .request(TestModel.class)
+                .requestMethod(GraphQLTemplate.GraphQLMethod.MUTATE)
+                .build();
+        assertEquals("GraphQLRequestEntity{request='mutation ($andAnothaVariable:status,$anothaVariable:Int,$andAListVariable:[String],$variableName:String!){ test (id:null) { testShort : testShort testCharacter testList { anotherTestString (variableName:$variableName) andAnothaOne (anothaVariable:$anothaVariable,andAnothaVariable:$andAnothaVariable,andAListVariable:$andAListVariable) } testInteger testBoolean nestedTest { anotherTestString (variableName:$variableName) andAnothaOne (anothaVariable:$anothaVariable,andAnothaVariable:$andAnothaVariable,andAListVariable:$andAListVariable) } testByte : testByte testString : testString (anotherOne:null,default:\"default\",defaultList:null) testArrayList testFloat testDouble testLong } } ', url='"+EXAMPLE_URL+"'}", requestEntity.toString());
+    }
+
+    @Test
+    public void simpleRequestWithVariables() throws MalformedURLException {
+        GraphQLRequestEntity requestEntity = GraphQLRequestEntity.Builder()
+                .url(EXAMPLE_URL)
+                .variables(new Variable<String>("variableName", "name"), new Variable<Integer>("anothaVariable", 1234), new Variable<STATUS>("andAnothaVariable", STATUS.active))
+                .request(TestModel.class)
+                .build();
+        assertEquals(EXAMPLE_URL, requestEntity.getUrl().toString());
+        assertEquals("GraphQLRequestEntity{request='query ($andAnothaVariable:status,$anothaVariable:Int,$andAListVariable:[String],$variableName:String!){ test (id:null) { testShort : testShort testCharacter testList { anotherTestString (variableName:$variableName) andAnothaOne (anothaVariable:$anothaVariable,andAnothaVariable:$andAnothaVariable,andAListVariable:$andAListVariable) } testInteger testBoolean nestedTest { anotherTestString (variableName:$variableName) andAnothaOne (anothaVariable:$anothaVariable,andAnothaVariable:$andAnothaVariable,andAListVariable:$andAListVariable) } testByte : testByte testString : testString (anotherOne:null,default:\"default\",defaultList:null) testArrayList testFloat testDouble testLong } } ', url='"+EXAMPLE_URL+"'}", requestEntity.toString());
+        assertEquals("{variableName=name, anothaVariable=1234, andAnothaVariable=active}", requestEntity.getVariables().toString());
+    }
+
+    @Test
+    public void simpleRequestWithScalars() throws MalformedURLException {
+        GraphQLRequestEntity requestEntity = GraphQLRequestEntity.Builder()
+                .url(EXAMPLE_URL)
+                .scalars(BigDecimal.class)
+                .request(TestModelScalar.class)
+                .build();
+        assertEquals(EXAMPLE_URL, requestEntity.getUrl().toString());
+        assertEquals(Collections.singletonList(BigDecimal.class), requestEntity.getScalars());
+        assertEquals("GraphQLRequestEntity{request='query { monay money } ', url='"+EXAMPLE_URL+"'}", requestEntity.toString());
+    }
+}
---
+++ americanexpress-nodes/5350218e34fcbfd8dc076053b1317ac2c2f5d938/4/DeserializerTest.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2018 American Express Travel Related Services Company, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.aexp.nodes.graphql;
+
+import io.aexp.nodes.graphql.internal.DefaultObjectMapperFactory;
+import io.aexp.nodes.graphql.internal.ObjectMapperFactory;
+import io.aexp.nodes.graphql.models.TestModel;
+import io.aexp.nodes.graphql.models.TestModelDateTime;
+import io.aexp.nodes.graphql.models.TestModels;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeserializerTest {
+
+    private ObjectMapperFactory objectMapperFactory;
+    private ObjectMapper mapper;
+
+    @Before
+    public void setup() {
+        objectMapperFactory = new DefaultObjectMapperFactory();
+        mapper = objectMapperFactory.newDeserializerMapper();
+    }
+
+    @Test
+    public void deserializeSimple() throws IOException {
+        String json = (
+            "{\n" +
+            "  \"test\": {\n" +
+            "    \"testString\": \"String\",\n" +
+            "    \"testByte\": \"1\",\n" +
+            "    \"testShort\": \"1\",\n" +
+            "    \"testInteger\": 1,\n" +
+            "    \"testLong\": 1,\n" +
+            "    \"testCharacter\": \"a\",\n" +
+            "    \"testFloat\": 1.5,\n" +
+            "    \"testDouble\": 1.5,\n" +
+            "    \"testBoolean\": true,\n" +
+            "    \"nestedTest\": {\n" +
+            "      \"anotherTestString\": \"AnotherString\"\n" +
+            "    },\n" +
+            "    \"testArrayList\": [\n" +
+            "      \"val1\",\n" +
+            "      \"val2\"\n" +
+            "    ],\n" +
+            "    \"testList\": [\n" +
+            "      {\n" +
+            "        \"anotherTestString\": \"AnotherString\"\n" +
+            "      }\n" +
+            "    ]\n" +
+            "  }\n" +
+            "}"
+        );
+        InputStream stream = new ByteArrayInputStream(json.getBytes());
+        JsonParser parser = mapper.getFactory().createParser(stream);
+        DeserializationContext ctxt = mapper.getDeserializationContext();
+        Deserializer<TestModel> deserializer = new Deserializer<TestModel>(TestModel.class, objectMapperFactory);
+        Resource<TestModel> res = deserializer.deserialize(parser, ctxt);
+        assertEquals("Resource{resource=TestTO{testString='String', testByte=1, testShort=1, testInteger=1, testLong=1, testCharacter=a, testFloat=1.5, testDouble=1.5, testBoolean=true, nestedTest=NestedTest{anotherTestString='AnotherString', andAnothaOne='null'}, testArrayList=[val1, val2], testList=[NestedTest{anotherTestString='AnotherString', andAnothaOne='null'}], ignoredField='null'}}", res.toString());
+    }
+
+    @Test
+    public void deserializeMulti() throws IOException {
+        String json = (
+            "{\n" +
+            "  \"test1\": {\n" +
+            "    \"testString\": \"String\",\n" +
+            "    \"testByte\": \"1\",\n" +
+            "    \"testShort\": \"1\",\n" +
+            "    \"testInteger\": 1,\n" +
+            "    \"testLong\": 1,\n" +
+            "    \"testCharacter\": \"a\",\n" +
+            "    \"testFloat\": 1.5,\n" +
+            "    \"testDouble\": 1.5,\n" +
+            "    \"testBoolean\": true,\n" +
+            "    \"nestedTest\": {\n" +
+            "      \"anotherTestString\": \"AnotherString\"\n" +
+            "    },\n" +
+            "    \"testArrayList\": [\n" +
+            "      \"val1\",\n" +
+            "      \"val2\"\n" +
+            "    ],\n" +
+            "    \"testList\": [\n" +
+            "      {\n" +
+            "        \"anotherTestString\": \"AnotherString\"\n" +
+            "      }\n" +
+            "    ]\n" +
+            "  },\n" +
+            "  \"test2\": {\n" +
+            "    \"testString\": \"String\",\n" +
+            "    \"testByte\": \"1\",\n" +
+            "    \"testShort\": \"1\",\n" +
+            "    \"testInteger\": 1,\n" +
+            "    \"testLong\": 1,\n" +
+            "    \"testCharacter\": \"a\",\n" +
+            "    \"testFloat\": 1.5,\n" +
+            "    \"testDouble\": 1.5,\n" +
+            "    \"testBoolean\": true,\n" +
+            "    \"nestedTest\": {\n" +
+            "      \"anotherTestString\": \"AnotherString\"\n" +
+            "    },\n" +
+            "    \"testArrayList\": [\n" +
+            "      \"val1\",\n" +
+            "      \"val2\"\n" +
+            "    ],\n" +
+            "    \"testList\": [\n" +
+            "      {\n" +
+            "        \"anotherTestString\": \"AnotherString\"\n" +
+            "      }\n" +
+            "    ]\n" +
+            "  }\n" +
+            "}"
+        );
+        InputStream stream = new ByteArrayInputStream(json.getBytes());
+        JsonParser parser = mapper.getFactory().createParser(stream);
+        DeserializationContext ctxt = mapper.getDeserializationContext();
+        Deserializer<TestModels> deserializer = new Deserializer<TestModels>(TestModels.class, objectMapperFactory);
+        Resource<TestModels> res = deserializer.deserialize(parser, ctxt);
+        assertEquals("Resource{resource=TestTOs{test1=TestTO{testString='String', testByte=1, testShort=1, testInteger=1, testLong=1, testCharacter=a, testFloat=1.5, testDouble=1.5, testBoolean=true, nestedTest=NestedTest{anotherTestString='AnotherString', andAnothaOne='null'}, testArrayList=[val1, val2], testList=[NestedTest{anotherTestString='AnotherString', andAnothaOne='null'}], ignoredField='null'}, test2=TestTO{testString='String', testByte=1, testShort=1, testInteger=1, testLong=1, testCharacter=a, testFloat=1.5, testDouble=1.5, testBoolean=true, nestedTest=NestedTest{anotherTestString='AnotherString', andAnothaOne='null'}, testArrayList=[val1, val2], testList=[NestedTest{anotherTestString='AnotherString', andAnothaOne='null'}], ignoredField='null'}}}", res.toString());
+    }
+
+    @Test
+    public void deserializeJsr310() throws IOException {
+        String json = (
+            "{\n" +
+            "  \"dateTime\": \"2018-10-29T22:00:01+00:00\"\n" +
+            "}"
+        );
+        InputStream stream = new ByteArrayInputStream(json.getBytes());
+        JsonParser parser = mapper.getFactory().createParser(stream);
+        DeserializationContext ctxt = mapper.getDeserializationContext();
+        Deserializer<TestModelDateTime> deserializer = new Deserializer<TestModelDateTime>(TestModelDateTime.class, objectMapperFactory);
+        Resource<TestModelDateTime> res = deserializer.deserialize(parser, ctxt);
+        assertEquals("Resource{resource=TestModelDateTime{dateTime='2018-10-29T22:00:01Z'}}", res.toString());
+    }
+}
