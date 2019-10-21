aa2613dea72e209c3b143e0512b3a0538dff9cd4
Fer Madeiral <fer.madeiral@gmail.com>
2019-10-21 15:11:26 -0400

    [Automatic commit] Errors from huaweicse/weathermap

---
+++ huaweicse-weathermap/11c78b60f830470bc3b985761eff4865c18fa841/27/TestCurrentweatherdata.java
@@ -0,0 +1,20 @@
+package com.service.weather.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCurrentweatherdata {
+
+	WeatherImpl currentweatherdataImpl;
+
+	@Before
+	public void setup() throws Exception {
+		currentweatherdataImpl = new WeatherImpl();
+	}
+
+	@Test
+	public void testhelloworld() {
+		
+	}
+
+}
