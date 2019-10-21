921c3ad0ce6de790910d0a37bf949ecd876e5983
Fer Madeiral <fer.madeiral@gmail.com>
2019-10-21 12:25:01 -0400

    [Automatic commit] Errors from gudaoxuri/dew

---
+++ gudaoxuri-dew/9213bcc4a2a4748f994b956a5bff6bf74b88cd88/0/WebController.java
@@ -0,0 +1,440 @@
+/*
+ * Copyright 2019. the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.trc.test.web;
+
+import com.ecfront.dew.common.$;
+import com.ecfront.dew.common.Resp;
+import com.ecfront.dew.common.StandardCode;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.JsonNode;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import ms.dew.Dew;
+import ms.dew.core.web.validation.CreateGroup;
+import ms.dew.core.web.validation.IdNumber;
+import ms.dew.core.web.validation.Phone;
+import ms.dew.core.web.validation.UpdateGroup;
+import org.junit.Assert;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Map;
+
+/**
+ * Web controller.
+ *
+ * @author gudaoxuri
+ */
+@RestController
+@Api(value = "测试")
+@RequestMapping(value = "/test/")
+@Validated
+public class WebController {
+
+    /**
+     * Hash 碰撞测试.
+     *
+     * @param body the body
+     * @return the void
+     */
+    @PostMapping(value = "hash-collision")
+    public Void validCreate(@RequestBody String body) {
+        long start = System.currentTimeMillis();
+        JsonNode json = $.json.toJson(body);
+        System.out.println("to Json Use time :" + ((System.currentTimeMillis() - start) / 1000));
+        start = System.currentTimeMillis();
+        $.json.toObject(body, UserDTO.class);
+        System.out.println("to User Use time :" + ((System.currentTimeMillis() - start) / 1000));
+        start = System.currentTimeMillis();
+        Map<String, Integer> map = $.json.toMap(json.get("dos"), String.class, Integer.class);
+        System.out.println("to Map Use time :" + ((System.currentTimeMillis() - start) / 1000));
+        return null;
+    }
+
+    /**
+     * Success resp.
+     *
+     * @param q the q
+     * @return the resp
+     */
+    @GetMapping(value = "success")
+    @ApiOperation(value = "success")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "q", value = "query", paramType = "query", dataType = "string", required = true),
+    })
+    public Resp<String> success(@RequestParam String q) {
+        return Resp.success("successful");
+    }
+
+    /**
+     * Bad request resp.
+     *
+     * @param q the q
+     * @return the resp
+     */
+    @GetMapping(value = "badRequest")
+    @ApiOperation(value = "badRequest")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "q", value = "query", paramType = "query", dataType = "string", required = true),
+    })
+    public Resp<String> badRequest(@RequestParam String q) {
+        return Resp.badRequest("badrequest");
+    }
+
+    /**
+     * Custom error resp.
+     *
+     * @param q the q
+     * @return the resp
+     * @throws Exception the exception
+     */
+    @GetMapping(value = "customError")
+    @ApiOperation(value = "customError")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "q", value = "query", paramType = "query", dataType = "string", required = true),
+    })
+    public Resp<String> customError(@RequestParam String q) throws Exception {
+        throw Dew.E.e("A000", new Exception("io error"));
+    }
+
+    /**
+     * Throws e resp.
+     *
+     * @param q the q
+     * @return the resp
+     * @throws Exception the exception
+     */
+    @GetMapping(value = "throws")
+    public Resp<String> throwsE(@RequestParam String q) throws Exception {
+        int i = 1 / 0;
+        return Resp.success(i + "");
+    }
+
+    /**
+     * Custom http state resp.
+     *
+     * @param q the q
+     * @return the resp
+     * @throws IOException the io exception
+     */
+    @GetMapping(value = "customHttpState")
+    @ApiOperation(value = "customHttpState")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "q", value = "query", paramType = "query", dataType = "string", required = true),
+    })
+    public Resp<String> customHttpState(@RequestParam String q) throws IOException {
+        throw Dew.E.e("A000", new IOException("io error"), StandardCode.UNAUTHORIZED);
+    }
+
+    /**
+     * Valid create user dto.
+     *
+     * @param userDTO the user dto
+     * @return the user dto
+     */
+    @PostMapping(value = "valid-create")
+    public UserDTO validCreate(@Validated(CreateGroup.class) @RequestBody UserDTO userDTO) {
+        return userDTO;
+    }
+
+    /**
+     * Valid update user dto.
+     *
+     * @param userDTO the user dto
+     * @return the user dto
+     */
+    @PostMapping(value = "valid-update")
+    public UserDTO validUpdate(@Validated(UpdateGroup.class) @RequestBody UserDTO userDTO) {
+        return userDTO;
+    }
+
+    /**
+     * Valid in method.
+     *
+     * @param age the age
+     * @return the string
+     */
+    @GetMapping(value = "valid-method-spring/{age}")
+    public String validInMethod(@Min(value = 2, message = "age必须大于2") @PathVariable("age") int age) {
+        return String.valueOf(age);
+    }
+
+    /**
+     * Valid in method.
+     *
+     * @param phone the phone
+     * @return the string
+     */
+    @GetMapping(value = "valid-method-own/{phone}")
+    public String validInMethod(@Phone @PathVariable("phone") String phone) {
+        return phone;
+    }
+
+    /**
+     * Error mapping.
+     *
+     * @return the string
+     */
+    @GetMapping(value = "error-mapping")
+    public String errorMapping() {
+        throw new AuthException();
+    }
+
+    /**
+     * Fallback test 1 resp.
+     *
+     * @return the resp
+     */
+    @GetMapping(value = "fallback1")
+    public Resp<String> fallbackTest1() {
+        return Resp.serverError("ssss").fallback();
+    }
+
+    /**
+     * Fallback test 2 resp.
+     *
+     * @return the resp
+     */
+    @GetMapping(value = "fallback2")
+    public Resp<String> fallbackTest2() {
+        throw new EmptyResultDataAccessException(1);
+    }
+
+    /**
+     * Time convert param resp.
+     *
+     * @param localDateTime the local date time
+     * @param localDate     the local date
+     * @param localTime     the local time
+     * @param instant       the instant
+     * @return the resp
+     */
+    @GetMapping("time/param")
+    public Resp<String> timeConvertParam(@RequestParam("date-time") LocalDateTime localDateTime,
+                                         @RequestParam("date") LocalDate localDate,
+                                         @RequestParam("time") LocalTime localTime,
+                                         @RequestParam("instant") Instant instant) {
+        Assert.assertNotNull(localDate);
+        Assert.assertNotNull(localDateTime);
+        Assert.assertNotNull(localTime);
+        Assert.assertNotNull(instant);
+        return Resp.success(null);
+    }
+
+    /**
+     * Time convert body resp.
+     *
+     * @param timeDTO the time dto
+     * @return the resp
+     */
+    @PostMapping("time/body")
+    public Resp<String> timeConvertBody(@RequestBody TimeDTO timeDTO) {
+        Assert.assertNotNull(timeDTO);
+        return Resp.success(null);
+    }
+
+    /**
+     * Time dto.
+     */
+    public static class TimeDTO {
+
+        private LocalTime localTime;
+
+        private LocalDate localDate;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime localDateTime;
+
+        private Instant instant;
+
+        /**
+         * Gets local time.
+         *
+         * @return the local time
+         */
+        public LocalTime getLocalTime() {
+            return localTime;
+        }
+
+        /**
+         * Sets local time.
+         *
+         * @param localTime the local time
+         */
+        public void setLocalTime(LocalTime localTime) {
+            this.localTime = localTime;
+        }
+
+        /**
+         * Gets local date.
+         *
+         * @return the local date
+         */
+        public LocalDate getLocalDate() {
+            return localDate;
+        }
+
+        /**
+         * Sets local date.
+         *
+         * @param localDate the local date
+         */
+        public void setLocalDate(LocalDate localDate) {
+            this.localDate = localDate;
+        }
+
+        /**
+         * Gets local date time.
+         *
+         * @return the local date time
+         */
+        public LocalDateTime getLocalDateTime() {
+            return localDateTime;
+        }
+
+        /**
+         * Sets local date time.
+         *
+         * @param localDateTime the local date time
+         */
+        public void setLocalDateTime(LocalDateTime localDateTime) {
+            this.localDateTime = localDateTime;
+        }
+
+        /**
+         * Gets instant.
+         *
+         * @return the instant
+         */
+        public Instant getInstant() {
+            return instant;
+        }
+
+        /**
+         * Sets instant.
+         *
+         * @param instant the instant
+         */
+        public void setInstant(Instant instant) {
+            this.instant = instant;
+        }
+    }
+
+    /**
+     * User dto.
+     */
+    public static class UserDTO {
+
+        @NotNull(groups = CreateGroup.class)
+        @IdNumber(message = "身份证号错误", groups = {CreateGroup.class, UpdateGroup.class})
+        private String idCard;
+
+        @Min(value = 10, groups = {CreateGroup.class, UpdateGroup.class})
+        private Integer age;
+
+        @NotNull(groups = CreateGroup.class)
+        @Phone(message = "手机号错误", groups = {CreateGroup.class, UpdateGroup.class})
+        private String phone;
+
+        private String addr;
+
+        /**
+         * Gets id card.
+         *
+         * @return the id card
+         */
+        public String getIdCard() {
+            return idCard;
+        }
+
+        /**
+         * Sets id card.
+         *
+         * @param idCard the id card
+         */
+        public void setIdCard(String idCard) {
+            this.idCard = idCard;
+        }
+
+        /**
+         * Gets age.
+         *
+         * @return the age
+         */
+        public Integer getAge() {
+            return age;
+        }
+
+        /**
+         * Sets age.
+         *
+         * @param age the age
+         */
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        /**
+         * Gets phone.
+         *
+         * @return the phone
+         */
+        public String getPhone() {
+            return phone;
+        }
+
+        /**
+         * Sets phone.
+         *
+         * @param phone the phone
+         */
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+
+        /**
+         * Gets addr.
+         *
+         * @return the addr
+         */
+        public String getAddr() {
+            return addr;
+        }
+
+        /**
+         * Sets addr.
+         *
+         * @param addr the addr
+         * @return the addr
+         */
+        public UserDTO setAddr(String addr) {
+            this.addr = addr;
+            return this;
+        }
+    }
+
+}
